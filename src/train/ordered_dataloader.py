cb2b16f04c9013941379480301288ee101baede5
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-04-23 08:54:11 +0800

    feat(train): ordered dataloader

--- src/train/ordered_dataloader.py
+++ src/train/ordered_dataloader.py
@@ -1,7 +1,30 @@
+from typing import Any
+import json
+from pathlib import Path
+from loguru import logger
+
+
 class CurriculumPromptLoader:
     def __init__(self, prompt_path: str) -> None:
-        pass
+        self.difficulty_to_prompts: dict[int, list[dict[str, Any]]] = {}
+        self.difficulty_to_prompts_idx: dict[int, int] = {}
+        # TODO: 没用calculator，直接从prompt_path中读取，未来可能修改
+        self.prompt_path = Path(prompt_path)
+        self.current_difficulty = 0
+        for difficulty_str, prompts in json.loads(self.prompt_path.read_text()).items():
+            self.difficulty_to_prompts[self._extract_difficulty(difficulty_str)] = prompts
+            self.difficulty_to_prompts_idx[self._extract_difficulty(difficulty_str)] = 0
 
-    def next(self): ...
+    def _extract_difficulty(self, difficulty_str: str) -> int:
+        return int(difficulty_str.split("_")[-1])
 
-    def current_complexity(self, complexity: int): ...
+    def next(self) -> tuple[str, Any]:
+        if self.difficulty_to_prompts_idx[self.current_difficulty] >= len(self.difficulty_to_prompts[self.current_difficulty]):
+            logger.warning(f"difficulty {self.current_difficulty} has no more prompts, reset to 0")
+            self.difficulty_to_prompts_idx[self.current_difficulty] = 0
+        prompt = self.difficulty_to_prompts[self.current_difficulty][self.difficulty_to_prompts_idx[self.current_difficulty]]
+        self.difficulty_to_prompts_idx[self.current_difficulty] += 1
+        return prompt
+
+    def set_difficulty(self, difficulty: int) -> None:
+        self.current_difficulty = difficulty
--- src/train/scorer.py
+++ src/train/scorer.py
@@ -1,27 +1,14 @@
 from typing import Any
 import torch
+from transformers.models.llava_next import LlavaNextProcessor, LlavaNextForConditionalGeneration
 
-# TODO: change the *** name
-imageqa_models = {
-    "llavav1.5-7b": (LLaVA, "llava-hf/llava-1.5-7b-hf"),
-    "llavav1.5-13b": ("LLaVA", "llava-hf/llava-1.5-13b-hf"),
-    "llavav1.6-7b": ("LLaVA", "llava-hf/llava-v1.6-vicuna-7b-hf"),
-    "llavav1.6-13b": ("LLaVA", "llava-hf/llava-v1.6-vicuna-13b-hf"),
-    "qwenvl": ("QwenVL", "Qwen/Qwen-VL"),
-    "qwenvl-chat": ("QwenVLChat", "Qwen/Qwen-VL-Chat"),
-    "internvl-chat-v1.5": ("InternVLChat", "failspy/InternVL-Chat-V1-5-quantable"),
-    "idefics2-8b": ("IDEFICS2", "HuggingFaceM4/idefics2-8b"),
-    "llavav1.5-7b-100-templated": ("LLaVA", "shijianS01/llava-v1.5-7b-lora-100-templated"),
-    "llavav1.5-7b-1k-templated": ("LLaVA", "shijianS01/llava-v1.5-7b-lora-1k-templated"),
-    "llavav1.5-7b-5k-templated": ("LLaVA", "shijianS01/llava-v1.5-7b-lora-5k-templated"),
-    "llavav1.5-7b-10k-templated": ("LLaVA", "shijianS01/llava-v1.5-7b-lora-10k-templated"),
-    "llavav1.5-7b-15k-templated": ("LLaVA", "shijianS01/llava-v1.5-7b-lora-15k-templated"),
-    "llavav1.5-13b-100-templated": ("LLaVA", "shijianS01/llava-v1.5-13b-lora-100-templated"),
-    "llavav1.5-13b-1k-templated": ("LLaVA", "shijianS01/llava-v1.5-13b-lora-1k-templated"),
-    "llavav1.5-13b-5k-templated": ("LLaVA", "shijianS01/llava-v1.5-13b-lora-5k-templated"),
-    "llavav1.5-13b-10k-templated": ("LLaVA", "shijianS01/llava-v1.5-13b-lora-10k-templated"),
-    "llavav1.5-13b-15k-templated": ("LLaVA", "shijianS01/llava-v1.5-13b-lora-15k-templated"),
-}
+
+class ImageQAModel:
+    def __init__(self, model_name: str, torch_device: str, enable_choice_search: bool, precision: torch.dtype) -> None:
+        self.model = LlavaNextForConditionalGeneration.from_pretrained(
+            model_name, torch_dtype=precision, low_cpu_mem_usage=True, device_map=torch_device
+        ).eval()
+        self.processor = LlavaNextProcessor.from_pretrained(model_name, device_map=torch_device)
 
 
 class VQAScorer:
