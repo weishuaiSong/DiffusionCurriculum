2928609a2438174dd8d4e59cea19b6e90a665a52
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-04-24 11:10:40 +0800

    fix(train): extract response and match ans

--- src/train/ordered_dataloader.py
+++ src/train/ordered_dataloader.py
@@ -5,13 +5,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class CurriculumPromptLoader:
     def __init__(self, prompt_path: str) -> None:
         self.difficulty_to_prompts: dict[int, list[dict[str, Any]]] = {}
         self.difficulty_to_prompts_idx: dict[int, int] = {}
         # TODO: 没用calculator，直接从prompt_path中读取，未来可能修改
         self.prompt_path = Path(prompt_path)
-        self.current_difficulty = 0
+        self.current_difficulty = 3
         for difficulty_str, prompts in json.loads(self.prompt_path.read_text()).items():
             self.difficulty_to_prompts[self._extract_difficulty(difficulty_str)] = prompts
             self.difficulty_to_prompts_idx[self._extract_difficulty(difficulty_str)] = 0
@@ -29,8 +30,7 @@ class CurriculumPromptLoader:
             self.difficulty_to_prompts_idx[self.current_difficulty]
         ]
         self.difficulty_to_prompts_idx[self.current_difficulty] += 1
-        return prompt["prompt"]
+        return prompt["prompt"], prompt
 
     def set_difficulty(self, difficulty: int) -> None:
         self.current_difficulty = difficulty
-
--- src/train/scorer.py
+++ src/train/scorer.py
@@ -3,13 +3,16 @@ import numpy as np
 import torch
 from torchvision.transforms import ToPILImage
 from transformers.pipelines import pipeline
-from bert_score import score as calc_bert_score
 from typing import Callable
+
+
+def is_answer_match(ans: str, should: str) -> bool:
+    return ans.lower() in should.lower()
 
 
 class VQAScorer:
     def __init__(self, vqa_model_name: str, set_curr_score: Callable[[int], None]) -> None:
-        self.vqa_pipeline = pipeline("image-text-to-text", model=vqa_model_name)
+        self.vqa_pipeline = pipeline("image-text-to-text", model=vqa_model_name, torch_dtype=torch.bfloat16)
         self.set_curr_score = set_curr_score
 
     def calc_score(self, images: torch.Tensor, prompts: tuple[str], metadata: tuple[Any]) -> torch.Tensor:
@@ -17,30 +20,29 @@ class VQAScorer:
         to_pil = ToPILImage()
 
         for i, image in enumerate(images):
-            qa: list[dict[str, str]] = metadata[i]["qa"]
+            qa: list[dict[str, str]] = metadata[i]["qa"]["object"]
             if isinstance(image, torch.Tensor):
                 image = to_pil(image)
-            response = self.vqa_pipeline(
-                text=[
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "type": "image",
-                                "image": image,
-                            },
-                            {
-                                "type": "text",
-                                "text": qa[i]["question"],
-                            },
-                        ],
-                    }
-                ]
-            )  # type: ignore
-            answer = response[0]["generated_text"]
-            _, _, bert_scores = calc_bert_score([answer], [qa[i]["answer"]], lang="en", verbose=True)
-            reward_score = bert_scores.mean().item() * 10  # type: ignore
-            score = np.array(reward_score)
-
+            score = 0
+            for each_qa in qa:
+                response = self.vqa_pipeline(
+                    text=[
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "image",
+                                    "image": image,
+                                },
+                                {
+                                    "type": "text",
+                                    "text": each_qa["question"],
+                                },
+                            ],
+                        }
+                    ]
+                )  # type: ignore
+                answer = response[0]["generated_text"][-1]["content"]
+                score += 1 / len(qa) if is_answer_match(answer, each_qa["answer"]) else 0
             scores.append(score)
-        return np.array(scores)  # type: ignore
+        return np.array(scores), None  # type: ignore
