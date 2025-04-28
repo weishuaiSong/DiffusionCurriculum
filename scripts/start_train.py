2f94c20b9fc653ea4ffb1d9bff4c4a4002a72367
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-04-28 10:26:42 +0800

    feat(train): support multiple train algo in param

--- scripts/start_train.py
+++ scripts/start_train.py
@@ -1,12 +1,31 @@
-from train.trainer import dpok
+from train.trainer import dpok, d3po, ddpo
 from train.train import CurriculumTrainerArguments, DiffusionCurriculumTrainer
 from transformers.hf_argparser import HfArgumentParser
+import sys
 
 
 def main():
-    parser = HfArgumentParser((CurriculumTrainerArguments, dpok.Config))  # type: ignore
-    curriculum_args, rl_args = parser.parse_args_into_dataclasses()
-
+    # 首先只解析课程学习参数，以确定使用哪个RL算法
+    parser = HfArgumentParser(CurriculumTrainerArguments)
+    curriculum_args, unknown_args = parser.parse_args_into_dataclasses(return_remaining_strings=True)
+    
+    # 重置sys.argv以包含未知参数，以便后续解析
+    sys.argv = [sys.argv[0]] + unknown_args
+    
+    # 根据选择的RL算法选择相应的Config类
+    if curriculum_args.rl_algorithm == "dpok":
+        ConfigClass = dpok.Config
+    elif curriculum_args.rl_algorithm == "d3po":
+        ConfigClass = d3po.Config
+    elif curriculum_args.rl_algorithm == "ddpo":
+        ConfigClass = ddpo.Config
+    else:
+        raise ValueError(f"不支持的RL算法: {curriculum_args.rl_algorithm}，支持的算法有: ddpo, d3po, dpok")
+    
+    # 解析RL特定参数
+    parser = HfArgumentParser(ConfigClass)
+    rl_args = parser.parse_args_into_dataclasses()[0]
+    
     trainer = DiffusionCurriculumTrainer(curriculum_args, rl_args)
     trainer.train()
 
--- src/train/train.py
+++ src/train/train.py
@@ -2,44 +2,57 @@ from dataclasses import dataclass, field
 from train.ordered_dataloader import CurriculumPromptLoader
 from train.scorer import VQAScorer
 from train.curriculum import Curriculum
-from train.trainer import dpok
+from train.trainer import dpok, d3po, ddpo
 
 
 @dataclass
 class CurriculumTrainerArguments:
     prompt_filename: str = field()
-    pretrained_model: str = field(
-        default="runwayml/stable-diffusion-v1-5", metadata={"help": "the pretrained model to use"}
-    )
     vqa_model: str = field(
         default="llava-hf/llava-v1.6-mistral-7b-hf", metadata={"help": "the pretrained model to use"}
     )
-    pretrained_revision: str = field(default="main", metadata={"help": "the pretrained model revision to use"})
-    hf_hub_model_id: str = field(
-        default="ddpo-finetuned-stable-diffusion", metadata={"help": "HuggingFace repo to save model weights to"}
-    )
-    use_lora: bool = field(default=True, metadata={"help": "Whether to use LoRA."})
     curriculum_strategy: str = field(
         default="random", metadata={"help": "The curriculum strategy to use for training."}
     )
     rl_algorithm: str = field(
-        default="ddpo",
+        default="dpok",
         metadata={"help": "The RL algorithm to use for training, supported algorithms: ddpo, d3po, dpok"},
     )
 
 
 class DiffusionCurriculumTrainer:
-    def __init__(self, curriculum_args: CurriculumTrainerArguments, rl_args: dpok.Config) -> None:
+    def __init__(self, curriculum_args: CurriculumTrainerArguments, rl_args) -> None:
         prompt_loader = CurriculumPromptLoader(prompt_path=curriculum_args.prompt_filename)
         scorer_ = VQAScorer(curriculum_args.vqa_model, prompt_loader.set_difficulty)
         self.curriculum = Curriculum(strategy=curriculum_args.curriculum_strategy)
-        self._trainer = dpok.Trainer(
-            curriculum=self.curriculum,
-            update_target_difficulty=prompt_loader.set_difficulty,
-            config=rl_args,
-            reward_function=scorer_.calc_score,
-            prompt_function=prompt_loader.next,
-        )
+
+        # 根据选定的RL算法初始化相应的训练器
+        if curriculum_args.rl_algorithm == "dpok":
+            self._trainer = dpok.Trainer(
+                curriculum=self.curriculum,
+                update_target_difficulty=prompt_loader.set_difficulty,
+                config=rl_args,
+                reward_function=scorer_.calc_score,
+                prompt_function=prompt_loader.next,
+            )
+        elif curriculum_args.rl_algorithm == "d3po":
+            self._trainer = d3po.Trainer(
+                curriculum=self.curriculum,
+                update_target_difficulty=prompt_loader.set_difficulty,
+                config=rl_args,
+                reward_function=scorer_.calc_score,
+                prompt_function=prompt_loader.next,
+            )
+        elif curriculum_args.rl_algorithm == "ddpo":
+            self._trainer = ddpo.Trainer(
+                curriculum=self.curriculum,
+                update_target_difficulty=prompt_loader.set_difficulty,
+                config=rl_args,
+                reward_function=scorer_.calc_score,
+                prompt_function=prompt_loader.next,
+            )
+        else:
+            raise ValueError(f"不支持的RL算法: {curriculum_args.rl_algorithm}，支持的算法有: ddpo, d3po, dpok")
 
     def train(self):
         self._trainer.train()
