46119a3a879f1ac0fb27df559e5bedd8d945ad17
MisakiWho <1932668007@qq.com>
2025-04-28 13:38:14 +0800

    修改scripts/generate_attributes.py

--- src/scene_graph_builder/__init__.py
+++ src/scene_graph_builder/__init__.py
@@ -1,3 +1,3 @@
 from .relation_generator import RelationGenerator, RelationGenerationConfig
-from .attribute_generator import AttributeGenerationConfig, AttributeConceptGenerator, AttributeValueGenerator
+from .attribute_generator import AttributeGenerationConfig, AttributeGenerator
 from .object_generator import ObjectGenerationConfig, ObjectGenerator
