455a48ef82f25e611c7fce99e70b9fae29d62cd5
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-05-05 01:41:30 +0800

    fix(ddpo): batch aggr into pipeline

--- src/train/scorer.py
+++ src/train/scorer.py
@@ -18,39 +18,46 @@ class VQAScorer:
         self.set_curr_score = set_curr_score
 
     def calc_score(self, images: torch.Tensor, prompts: tuple[str], metadata: tuple[Any]) -> torch.Tensor:
+        batch_size = len(images)
         scores = []
         to_pil = ToPILImage()
 
+        all_qa = []
         for i, image in enumerate(images):
             qa: list[dict[str, str]] = (
                 metadata[i]["qa"]["object"] + metadata[i]["qa"]["relation"] + metadata[i]["qa"]["attribute"]
             )
             if isinstance(image, torch.Tensor):
                 image = to_pil(image.to(torch.float))
-            score = 0
-            all_qa = []
             for each_qa in qa:
                 all_qa.append(
-                    [
-                        {
-                            "role": "user",
-                            "content": [
-                                {
-                                    "type": "image",
-                                    "image": image,
-                                },
-                                {
-                                    "type": "text",
-                                    "text": each_qa["question"],
-                                },
-                            ],
-                        }
-                    ]
+                    (
+                        [
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "image",
+                                        "image": image,
+                                    },
+                                    {
+                                        "type": "text",
+                                        "text": each_qa["question"],
+                                    },
+                                ],
+                            }
+                        ],
+                        each_qa["answer"],
+                        len(qa),
+                    )
                 )
-            response = self.vqa_pipeline(text=all_qa)  # type: ignore
+        for i in range(0, len(all_qa), batch_size):
+            q_with_contents, answers, qa_lens = zip(*all_qa[i : i + batch_size])
+            response = self.vqa_pipeline(text=q_with_contents)  # type: ignore
 
+            score = 0
             for i, resp in enumerate(response):
                 answer = resp[0]["generated_text"][-1]["content"]
-                score += 1 / len(qa) if is_answer_match(answer, qa[i]["answer"]) else 0
+                score += 1 / qa_lens[i] if is_answer_match(answer, answers[i]) else 0
             scores.append(score)
         return np.array(scores), None  # type: ignore
