7c0c89e6255ab60b0e4b6a451b0e2dd5b8dee4fa
zqqqqz2000 <zqqqqz2000@sina.cn>
2025-05-13 01:58:28 +0800

    fix(ddpo): convert reward to numpy

--- src/train/trainer/ddpo.py
+++ src/train/trainer/ddpo.py
@@ -408,7 +408,11 @@ class Trainer:
             rewards, reward_metadata = self.reward_fn(self.vqa_pipeline, images, prompts, prompt_metadata)
             rewards = torch.as_tensor(rewards, device=self.accelerator.device)
             self.last_difficulty = self.curriculum.infer_target_difficulty(
-                {"current_step": global_step + i, "difficulty": self.last_difficulty, "reward": rewards.mean()}
+                {
+                    "current_step": global_step + i,
+                    "difficulty": self.last_difficulty,
+                    "reward": rewards.mean().cpu().numpy(),
+                }
             )
             self.update_target_difficulty(self.last_difficulty)
 
